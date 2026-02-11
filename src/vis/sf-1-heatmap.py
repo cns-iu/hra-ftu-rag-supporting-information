11a1b1e7a6b7b2fd33d50aaca3949f28edcfaec2
yokongkk <kongyx0515@gmail.com>
2026-02-11 12:36:26 -0500

    Update the sf 1 & 2

---
+++ src/vis/sf-1-heatmap.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+mpl.rcParams.update({
+    'font.family': 'sans-serif',
+    'font.sans-serif': ['Arial'],
+    'font.size': 10,
+    'axes.labelsize': 10,
+    'axes.titlesize': 10,
+    'xtick.labelsize': 9,
+    'ytick.labelsize': 9,
+    'legend.fontsize': 9,
+    'axes.linewidth': 0.8,
+    'xtick.major.width': 0.8,
+    'ytick.major.width': 0.8,
+    'xtick.major.size': 3,
+    'ytick.major.size': 3
+})
+
+# visualization: age
+df = pd.read_csv(r"data\vis-source-data\sf-1a-age.csv")
+df = df.set_index("FTU").fillna(0)
+
+plt.figure(figsize=(7, 7))
+im = plt.imshow(df.values, cmap="YlGnBu", vmin=0)
+plt.imshow(df.values, cmap="YlGnBu", vmin=0)
+plt.yticks(range(len(df.index)), df.index)
+plt.xticks(range(len(df.columns)), df.columns, rotation=90)
+cbar = plt.colorbar(im, label="#Records", shrink=0.6)  
+plt.xlabel("Age group")
+plt.ylabel("FTU")
+
+plt.tight_layout()
+plt.savefig(r"vis\sf-1a-age.svg", bbox_inches='tight')
+plt.close()
+
+
+# visualization: sex
+df = pd.read_csv(r"data\vis-source-data\sf-1b-sex.csv")
+df = df.set_index("FTU").fillna(0)
+
+plt.figure(figsize=(7, 7))
+im = plt.imshow(df.values, cmap="YlGnBu", vmin=0)
+plt.imshow(df.values, cmap="YlGnBu", vmin=0)
+plt.yticks(range(len(df.index)), df.index)
+plt.xticks(range(len(df.columns)), df.columns, rotation=90)
+cbar = plt.colorbar(im, label="#Records", shrink=0.6)  
+plt.xlabel("Sex")
+plt.ylabel("FTU")
+
+plt.tight_layout()
+plt.savefig(r"vis\sf-1b-sex.svg", bbox_inches='tight')
+plt.close()
+
+# visualization: BMI
+df = pd.read_csv(r"data\vis-source-data\sf-1c-bmi.csv")
+df = df.set_index("FTU").fillna(0)
+
+plt.figure(figsize=(7, 7))
+im = plt.imshow(df.values, cmap="YlGnBu", vmin=0)
+plt.yticks(range(len(df.index)), df.index)
+plt.xticks(range(len(df.columns)), df.columns, rotation=90)
+cbar = plt.colorbar(im, label="#Records", shrink=0.6)  
+plt.xlabel("BMI")
+plt.ylabel("FTU")
+
+plt.tight_layout()
+plt.savefig(r"vis\sf-1c-bmi.svg", bbox_inches='tight')
+plt.close()
+
+
---
+++ src/vis/sf-2-ftu-sb-it-cnt.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+mpl.rcParams.update({
+    'font.family': 'sans-serif',
+    'font.sans-serif': ['Arial'],
+    'font.size': 10,
+    'axes.labelsize': 10,
+    'axes.titlesize': 10,
+    'xtick.labelsize': 9,
+    'ytick.labelsize': 9,
+    'legend.fontsize': 9,
+    'axes.linewidth': 0.8,
+    'xtick.major.width': 0.8,
+    'ytick.major.width': 0.8,
+    'xtick.major.size': 3,
+    'ytick.major.size': 3
+})
+
+file_path = r"data\vis-source-data\sf-2-ftu-sb-it-cnt.csv"
+df = pd.read_csv(file_path)
+
+df["micro_scaled"] = df["micro_yes_count"] / 1000
+df["schema_scaled"] = df["schema_yes_count"] / 1000
+
+
+fig, ax = plt.subplots(figsize=(7, 7)) 
+
+y_positions = range(len(df))
+
+ax.barh(
+    y_positions,
+    df["micro_scaled"],
+    color="#156082",
+    label="#microscopy images"
+)
+
+ax.barh(
+    y_positions,
+    df["schema_scaled"],
+    left=df["micro_scaled"],
+    color="#e97132",
+    label="#schematic images"
+)
+
+ax.set_yticks(y_positions)
+ax.set_yticklabels(df["ftu"])
+ax.invert_yaxis() 
+
+max_total = (df["micro_scaled"] + df["schema_scaled"]).max()
+ax.set_xlim(0, max_total * 1.05)
+
+ax.set_xlabel("The number of images (thousand)")
+ax.set_ylabel("FTU")
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+ax.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
+
+ax.legend(frameon=False)
+
+plt.tight_layout()
+
+plt.savefig(r"vis\sf-2-ftu-sb-it.svg", bbox_inches='tight')
+
+plt.close()
